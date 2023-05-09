05c0512f6840dc6940dd56cb3442730c4d46a999
Flovedev <flove.dev@gmail.com>
2023-05-09 13:59:54 +0100

    fixing displays

--- src/components/main/Over.tsx
+++ src/components/main/Over.tsx
@@ -17,62 +17,64 @@ const Over = (props: IProps) => {
   //   console.log(currentUser);
   //   console.log(currentUserToken);
   return (
-    <div className="d-flex flex-column align-items-center overButtons pb-2">
-      <div
-        className="pointer mb-1"
-        onClick={(e) => {
-          e.preventDefault();
-          dispatch(overRequest(currentUserToken, "favourites", props.data));
-        }}
-      >
-        {currentUser?.games?.favourites?.some(
-          (e: IOver) => e.id === props.data.id
-        ) ? (
-          <AiFillStar size={40} color="yellow" />
-        ) : (
-          <AiOutlineStar size={40} color="grey" />
-        )}
-      </div>
-      <div className="d-flex">
+    currentUserToken && (
+      <div className="d-flex flex-column align-items-center overButtons pb-2">
         <div
+          className="pointer mb-1"
           onClick={(e) => {
             e.preventDefault();
-            dispatch(overRequest(currentUserToken, "pending", props.data));
+            dispatch(overRequest(currentUserToken, "favourites", props.data));
           }}
         >
-          {currentUser?.games?.pending?.some(
+          {currentUser?.games?.favourites?.some(
             (e: IOver) => e.id === props.data.id
           ) ? (
-            <Button variant="warning" className="py-0 pr-1 pl-4 pb-1">
-              toPlay
-            </Button>
+            <AiFillStar size={40} color="yellow" />
           ) : (
-            <Button variant="secondary" className="py-0 pr-1 pl-4 pb-1">
-              toPlay
-            </Button>
+            <AiOutlineStar size={40} color="grey" />
           )}
         </div>
-        <div
-          className="ml-1"
-          onClick={(e) => {
-            e.preventDefault();
-            dispatch(overRequest(currentUserToken, "over", props.data));
-          }}
-        >
-          {currentUser?.games?.over?.some(
-            (e: IOver) => e.id === props.data.id
-          ) ? (
-            <Button variant="success" className="py-0 pl-1 pb-1 pr-4">
-              Over!!
-            </Button>
-          ) : (
-            <Button variant="secondary" className="py-0 pl-1 pb-1 pr-4">
-              Over!!
-            </Button>
-          )}
+        <div className="d-flex">
+          <div
+            onClick={(e) => {
+              e.preventDefault();
+              dispatch(overRequest(currentUserToken, "pending", props.data));
+            }}
+          >
+            {currentUser?.games?.pending?.some(
+              (e: IOver) => e.id === props.data.id
+            ) ? (
+              <Button variant="warning" className="py-0 pr-1 pl-4 pb-1">
+                toPlay
+              </Button>
+            ) : (
+              <Button variant="secondary" className="py-0 pr-1 pl-4 pb-1">
+                toPlay
+              </Button>
+            )}
+          </div>
+          <div
+            className="ml-1"
+            onClick={(e) => {
+              e.preventDefault();
+              dispatch(overRequest(currentUserToken, "over", props.data));
+            }}
+          >
+            {currentUser?.games?.over?.some(
+              (e: IOver) => e.id === props.data.id
+            ) ? (
+              <Button variant="success" className="py-0 pl-1 pb-1 pr-4">
+                Over!!
+              </Button>
+            ) : (
+              <Button variant="secondary" className="py-0 pl-1 pb-1 pr-4">
+                Over!!
+              </Button>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
